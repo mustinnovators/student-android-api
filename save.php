1505548da5fedaca5d1520f0950515d797db0db7
Eric Kirima <ekirima@gmail.com>
2019-03-20 19:55:31 +0300

    make the api android ready

--- save.php
+++ save.php
@@ -1,18 +1,22 @@
 <?php
 $connection = mysqli_connect("localhost",'root','root',"innovate_db");
 if(isset($_POST)){
+	//echo json_encode(['message'=> "".json_encode($_POST)]);
     extract($_POST);
 
     $sql = "INSERT INTO  students(FullName,Email,Phone,Status)
             values('$fullname','$email','$phone',$status)";
-    echo $sql;
+    //echo $sql;
     $result = mysqli_query($connection,$sql);
     if(!$result){
-        echo "An error occurred: ".mysqli_error($connection);
+        echo json_encode(['message'
+		=>"An error occurred: ".mysqli_error($connection)]);
     }else{
-        echo "Information saved successfully!";
+        //echo "Information saved successfully!";
+	echo json_encode(['message'=> "Successfully saved"]);
     }
-}else{
-    echo "You have not provided any values";
 
+}else{
+    //echo "You have not provided any values";
+	echo json_encode(['message'=> "you cant save emppty values"]);
 }
